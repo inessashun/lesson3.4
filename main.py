2079bcc033b090ddfac4e82a49012b4e8873036c
inessashun <inshun2018@gmail.com>
2024-05-03 00:51:32 +0300

    add img names

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # set screen size
 
 pygame.display.set_caption("Game Target")  # screen name
-icon = pygame.image.load("img/аватар.jpg")  # screen image
+icon = pygame.image.load("img/avatar.jpg")  # screen image
 pygame.display.set_icon(icon)  # set icon
 
-target_img = pygame.image.load("img/target.png")
-target_width = 80
-target_height = 80
+target_img = pygame.image.load("img/heart.png")
+target_width = 50
+target_height = 50
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
